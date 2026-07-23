ad111095b14fd5c593df6661d64af1fe93e8671d
Harsh seth <68950338+HarshSeth11@users.noreply.github.com>
2026-07-23 11:41:58 +0530

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int n = intervals.size();
+        if(n == 0 || n == 1) return n;
+
+        sort(intervals.begin(), intervals.end(), [](const Interval& a, const Interval& b) {
+            return a.start < b.start;
+        });
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+        pq.push(intervals[0].end);
+
+        for(int i=1; i<n; i++) {
+            if(pq.top() <= intervals[i].start) 
+                pq.pop();
+            
+            pq.push(intervals[i].end);
+        }
+
+        return pq.size();
+    }
+};
